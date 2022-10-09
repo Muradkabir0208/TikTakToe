6108f6e45828171bb9c9c5809740d5c1b9be9fd4
gintoki <satyamrocker1234@gmail.com>
2022-10-09 16:36:18 +0530

    removed TcpServer from client

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,36 +2,6 @@
 #include <iomanip>
 #include <iostream>
 
-void runTcpServer(unsigned short port)
-{
-    // Create a server socket to accept new connections
-    sf::TcpListener listener;
-
-    // Listen to the given port for incoming connections
-    if (listener.listen(port) != sf::Socket::Status::Done)
-        return;
-    std::cout << "Server is listening to port " << port << ", waiting for connections... " << std::endl;
-
-    // Wait for a connection
-    sf::TcpSocket socket;
-    if (listener.accept(socket) != sf::Socket::Status::Done)
-        return;
-    std::cout << "Client connected: " << socket.getRemoteAddress().toString() << std::endl;
-
-    // Send a message to the connected client
-    const char out[] = "Hi, I'm the server";
-    if (socket.send(out, sizeof(out)) != sf::Socket::Status::Done)
-        return;
-    std::cout << "Message sent to the client: " << std::quoted(out) << std::endl;
-
-    // Receive a message back from the client
-    char in[128];
-    std::size_t received;
-    if (socket.receive(in, sizeof(in), received) != sf::Socket::Status::Done)
-        return;
-    std::cout << "Answer received from the client: " << std::quoted(in) << std::endl;
-}
-
 void runTcpClient(unsigned short port)
 {
     sf::IpAddress server;
@@ -62,19 +32,20 @@ void runTcpClient(unsigned short port)
 int main()
 {
     unsigned short port = 5000;
-    char who;
-    std::cout << "Enter what to run as s for server c for client " << std::endl;
-    std::cin >> who;
-    if (who == 's')
-    {
-        // server handler
-        runTcpServer(port);
-    }
-    else
-    {
-        // client handler
-        runTcpClient(port);
-    }
+    // char who;
+    // std::cout << "Enter what to run as s for server c for client " << std::endl;
+    // std::cin >> who;
+    // if (who == 's')
+    // {
+    //     // server handler
+    //     runTcpServer(port);
+    // }
+    // else
+    // {
+    //     // client handler
+    //     runTcpClient(port);
+    // }
+    runTcpClient(port);
 
     std::cout << "Press enter to exit..." << std::endl;
     std::cin.ignore(10000, '\n');
